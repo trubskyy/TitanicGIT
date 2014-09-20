8fcb315b080b959df3cdb05d60422cc7051ac225
trubskyy <mykhaylo@trubskyy.com>
2014-09-19 21:42:35 -0400

    Files Added

---
+++ Code/Titanic.R
@@ -0,0 +1,12 @@
+
+library("ggplot2")
+setwd("C:/Users/mykha_000/SkyDrive/Kaggle/Titanic")
+
+data.dir <- ".//Data"
+
+titanic <- read.csv(file = ".//Data//train.csv",stringsAsFactors = FALSE)
+
+class <- factor(titanic[,"Pclass"])
+
+linear <- lm(data=titanic, Survived ~ Pclass + Sex)
+
